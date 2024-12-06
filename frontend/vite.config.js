109d6172ce2219fe58ff19d3dd135ffd216c1dbb
Enstrue <Ens.true@gmail.com>
2024-12-06 11:55:06 +0300

    try to repair deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',  // Измените папку сборки на `build`
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:5001', // Прокси для запросов API
