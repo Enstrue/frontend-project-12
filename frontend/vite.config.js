e239396c5a430f8920aaf312ea431b18a186e3f5
Enstrue <Ens.true@gmail.com>
2024-12-06 15:15:26 +0300

    step 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,19 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+
   build: {
-    outDir: 'build',  // Измените папку сборки на `build`
+    outDir: 'build',
+    rollupOptions: {
+      input: {
+        main: '/src/index.jsx',
+      },
+    },
   },
+
   server: {
     proxy: {
-      '/api': 'http://localhost:5001', // Прокси для запросов API
+      '/api': 'http://localhost:5001',
     },
   },
 });
