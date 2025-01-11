b32ee64490ba1e2917653d43dd3db82e7d69052f
Enstrue <Ens.true@gmail.com>
2025-01-11 19:51:31 +0300

    fix autotests

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -123,6 +123,7 @@ const ChatPage = () => {
               <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"></path>
               <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"></path>
             </svg>
+            <span className="visually-hidden">+</span>
             </Button>
           </h4>
 
@@ -222,9 +223,13 @@ const ChatPage = () => {
               <Modal.Body>
                 <Field
                   name="name"
+                  id="name"
                   placeholder=""
                   className={`form-control ${touched.name && errors.name ? 'is-invalid' : touched.name ? 'is-valid' : ''}`}
                 />
+                <label htmlFor="name" className="visually-hidden">
+                  Имя канала
+                </label>
                 {touched.name && errors.name && (
                   <div className="invalid-feedback">{errors.name}</div>
                 )}
