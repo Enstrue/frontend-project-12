73d7dfd1509e2e1ab5f464ed9cc8769560832e28
Enstrue <Ens.true@gmail.com>
2025-01-13 17:00:07 +0300

    123

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -76,7 +76,7 @@ const ChatPage = () => {
     try {
       const cleanedName = leoProfanity.clean(values.name);
       await dispatch(addNewChannel(cleanedName)).unwrap();
-      toast.success(t("chat.notifications.channelCreated"));
+      toast.success("Канал создан");
       resetForm();
       setModalType(null);
     } catch {
@@ -122,8 +122,8 @@ const ChatPage = () => {
 
   const validationSchema = Yup.object({
     name: Yup.string()
-      .min(3, t("validation.usernameMin"))
-      .max(20, t("validation.usernameMax"))
+      .min(3, t("validation.username"))
+      .max(20, t("validation.username"))
       .required(t("signup.validation.required"))
       .test(
         "unique",
@@ -134,13 +134,17 @@ const ChatPage = () => {
 
   return (
     <div className="container py-4">
-      <ToastContainer position="top-right" autoClose={3000} />
+      <ToastContainer position="top-right" autoClose={3000000} />
       <div className="row">
         <div className="col-md-4 border-end">
           <h4 className="d-flex justify-content-between align-items-center">
             {t("chat.channels")}
-            <Button variant="primary" onClick={() => setModalType("add")}>
-              +
+            <Button variant="primary" onClick={() => setModalType('add')}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" className="me-1">
+                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"></path>
+                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"></path>
+              </svg>
+              <span className="visually-hidden">+</span>
             </Button>
           </h4>
 
