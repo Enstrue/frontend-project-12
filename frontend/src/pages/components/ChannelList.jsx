ab1abe9dd233b603da92c3b108ac5e85355f0be1
Enstrue <Ens.true@gmail.com>
2025-01-27 14:10:28 +0300

    fix autotests

--- frontend/src/pages/components/ChannelList.jsx
+++ frontend/src/pages/components/ChannelList.jsx
@@ -1,5 +1,4 @@
-// components/ChannelList.js
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Dropdown } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 
@@ -7,11 +6,21 @@ const ChannelList = ({
   channels, currentChannel, setCurrentChannel, setModalType, setSelectedChannel,
 }) => {
   const { t } = useTranslation();
+  const channelListRef = useRef(null);
 
   const isProtectedChannel = (channel) => ['general', 'random'].includes(channel.name);
 
+  useEffect(() => {
+    if (channelListRef.current) {
+      const { scrollHeight, clientHeight } = channelListRef.current;
+      channelListRef.current.scrollTop = scrollHeight - clientHeight;
+    }
+  }, [channels]);
+
   return (
-    <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
+    <div
+      className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex"
+    >
       <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
         <b>{t('chat.channels')}</b>
         <button
@@ -24,10 +33,14 @@ const ChannelList = ({
             <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
           </svg>
+          <span className="visually-hidden">+</span>
         </button>
       </div>
 
-      <ul className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
+      <ul
+        className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block"
+        ref={channelListRef}
+      >
         {channels.map((channel) => {
           const isActive = currentChannel === channel.id;
           const hasDropdown = !isProtectedChannel(channel);
